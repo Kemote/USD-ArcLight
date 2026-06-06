d70530f05419c9dce6056aee18d75bd61862cec1
Tomasz <tomek.szymski@gmail.com>
2026-06-06 17:37:07 +0200

    fix some problems

--- arclight/core/usd_engine.py
+++ arclight/core/usd_engine.py
@@ -1,12 +1,10 @@
 import os
-from pxr import Usd, UsdUtils
+from pxr import Usd, UsdUtils, Pcp
 
 
 def open_layer(path):
-    layer = None
-    if os.path.exists(path):
-        with Usd.StageCacheContext(UsdUtils.StageCache.Get()):
-            layer = Usd.Stage.Open(path)
+    with Usd.StageCacheContext(UsdUtils.StageCache.Get()):
+        layer = Usd.Stage.Open(path)
     return layer
 
 def create_in_memmory_stage():
@@ -21,7 +19,31 @@ def get_prim_compostion_data(prim: Usd.Prim):
 
 class PrimStackInfo:
     def __init__(self, arc):
-        self.arc_type = arc.GetArcType()
-        self.intro_layer = arc.GetIntroducingLayer()
+        self.arc_type = self._get_arc_type(arc)
+        self.intro_layer = self._get_layer_real_path(arc)
         self.intro_prim = arc.GetIntroducingPrimPath()
         self.is_implicit = arc.IsImplicit()
+
+    @staticmethod
+    def _get_arc_type(arc):
+        arc_type = arc.GetArcType()
+        if arc_type == Pcp.ArcTypeRoot:
+            return "Sublayer / Local"
+        elif arc_type == Pcp.ArcTypeInherit:
+            return "Inherit"
+        elif arc_type == Pcp.ArcTypeVariant:
+            return "Variant"
+        elif arc_type == Pcp.ArcTypeReference:
+            return "Reference"
+        elif arc_type == Pcp.ArcTypePayload:
+            return "Payload"
+        elif arc_type == Pcp.ArcTypeSpecialize:
+            return "Specializes"
+        
+    @staticmethod
+    def _get_layer_real_path(arc):
+        intro_layer = arc.GetIntroducingLayer()
+        if intro_layer:
+            return intro_layer.realPath
+        else:
+            return "annonymous layer"
--- arclight/widgets/layers_stack_widget/layer_stack.py
+++ arclight/widgets/layers_stack_widget/layer_stack.py
@@ -1,5 +1,11 @@
 import os
-from PySide6.QtWidgets import QListWidget, QAbstractItemView
+from PySide6.QtCore import Signal
+from PySide6.QtWidgets import (QListWidget, 
+                               QAbstractItemView, 
+                               QWidget, 
+                               QVBoxLayout,
+                               QHBoxLayout, 
+                               QPushButton)
 
 
 class LayerStackItem:
@@ -11,13 +17,65 @@ class LayerStackItem:
         return self.name
 
 
-class LayerStackWidget(QListWidget):
+class LayerStackWidget(QWidget):
+    load_sublayer_signal = Signal()
+    delete_signal = Signal(bool)
+    
+    def __init__(self):
+        super().__init__()
+        self.list = LayerStackListWidget()
+              
+        load_btn = QPushButton("Add sublayer")
+        load_btn.clicked.connect(self._add_sublayer)
+        delete_btn = QPushButton("Delete selected")
+        delete_btn.clicked.connect(self._delete)
+        panel_layout = QHBoxLayout()
+        panel_layout.addWidget(load_btn)
+        panel_layout.addWidget(delete_btn)
+
+        widget_layout = QVBoxLayout()
+        widget_layout.addLayout(panel_layout)
+        widget_layout.addWidget(self.list)
+        
+        self.setLayout(widget_layout)
+
+    @property
+    def items_count(self):
+        return self.list.count()
+    
+    @property
+    def list_model(self):
+        return self.list.model()
+
+    def add_layer(self, file_path):
+        self.list.add_layer(file_path)
+
+    def get_item_by_index(self, index):
+        return self.list.item(index)
+
+    def get_layer_item(self, list_item):
+        return self.list.get_layer_item(list_item.text())
+    
+    def _add_sublayer(self):
+        self.load_sublayer_signal.emit()
+
+    def _delete(self):
+        for index in self.list.selectedIndexes():
+            self.list.takeItem(index.row())
+        self.delete_signal.emit(True)
+
+    def delete_all(self):
+        self.list.clear()
+
+
+class LayerStackListWidget(QListWidget):
     def __init__(self):
         super().__init__()
         self._stack = {}
         self.setDragEnabled(True)
         self.setAcceptDrops(True)
         self.setDropIndicatorShown(True)
+        self.setSelectionMode(QAbstractItemView.SelectionMode.ExtendedSelection)
         self.setDragDropMode(QAbstractItemView.DragDropMode.InternalMove)
 
     def add_layer(self, path):
